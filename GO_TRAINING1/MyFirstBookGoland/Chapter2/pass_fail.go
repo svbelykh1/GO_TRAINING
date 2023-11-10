5d9dee81af7ad0e7e5b27d6abb775558e8f940ff
svbelykh1 <b.s.v.belykh@gmail.com>
2023-11-10 14:29:56 +0300

    Chapter2 обновлена из Chapter4(Packets)

--- GO_TRAINING1/MyFirstBookGoland/Chapter2/pass_fail.go
+++ GO_TRAINING1/MyFirstBookGoland/Chapter2/pass_fail.go
@@ -23,31 +23,36 @@ func main() {
 	*/
 
 	//получаем ввод с клавиатуры в переменную input
-
-	fmt.Print("Введите любую строку  ")
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//1) Вариант использовать Replace
-	//replacer := strings.NewReplacer("\n", "")
-	//rInput := replacer.Replace(input)
-	//2) использовать TrimSpace у пакета strings, которая удалит все пробеоы табуляции и переносы строк
-
-	rInput := strings.TrimSpace(input)
-	//fmt.Println(rInput)
-	toRInput, err := strconv.ParseFloat(rInput, 64)
-	if err != nil {
-		log.Fatal(err)
-	}
 	var status string
-	if toRInput >= 60 {
+	fmt.Print("Введите число: \n")
+	input, err := getfloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Println(input)
+
+	if input >= 60 {
 		status = "passing"
 	} else {
 		status = "failing"
 	}
 	fmt.Println(status)
+}
 
+func getfloat() (float64, error) {
+	//ожидание и обработка ввода с клавиатуры
+	reader := bufio.NewReader(os.Stdin)
+	inp, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+		return 0, err
+	}
+	//обработка введенного значения, использовать TrimSpace у пакета strings, которая удалит все пробеоы табуляции и переносы строк
+	inp = strings.TrimSpace(inp)
+	//
+	toFloatInput, err := strconv.ParseFloat(inp, 64)
+	if err != nil {
+		return 0, err
+	}
+	return toFloatInput, nil
 }
